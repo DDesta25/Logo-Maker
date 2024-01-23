5c840d47a6fe6cae092d8b728dbeb456bf42b8de
DDesta25 <145198978+DDesta25@users.noreply.github.com>
2024-01-23 10:06:44 -0500

    added svg logic currently reciving error:=Something else went wrong

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-// const{Shape, Square,Triangle,Circle} = require("./lib/shapes");
-
+const { Shape, Square, Triangle, Circle } = require("./lib/shapes");
 
 const questions = [
- 
   {
     type: "input",
     name: "textColor",
@@ -16,9 +14,9 @@ const questions = [
     type: "list",
     name: "shape",
     message: "which of the fallowing shapes would you like?",
-    choices: ['Square','Triangle','Circle']
+    choices: ["Square", "Triangle", "Circle"],
   },
-  
+
   {
     type: "input",
     name: "text",
@@ -28,53 +26,55 @@ const questions = [
     type: "input",
     name: "shapeColor",
     message: "what color should the shape-color be?",
-  }]
-  
-  function writeToFile(fileName, data) {
-  
-    
-    return fs.writeFileSync(fileName, data);
-  }
+  },
+];
+
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(fileName, data);
+}
 // text-color,shape,text,shape-color
-  function init(questions) {
-    inquirer
-      .prompt(
-        /* Pass your questions in here */
-        questions
-      )
-      .then(({textColor,shape,text,shapeColor}) => {
-        console.log(textColor,shape,text,shapeColor);
-       
-        switch (shape) {
-            case "Square":
-            //   shape = new Case1();
-            console.log('Square');
-              break;
-  
-            case "Triangle":
-            //   shape = new Case2();
-            console.log('Triangle');
-              break;
-  
-            default:
-            //   shape = new Case3();
-            console.log('Circle');
-              break;
-          }
-          const svg = new SVG()
-        // Use user feedback for... whatever!!
-        writeToFile("shape.svg", svg.render({...answers}));
-      })
-      .catch((error) => {
-        if (error.isTtyError) {
-          // Prompt couldn't be rendered in the current environment
-          console.log("Prompt couldn't be rendered in the current environment");
-        } else {
-          // Something else went wrong
-          console.log("Something else went wrong");
-        }
-      });
-  }
-  
-  // Function call to initialize app-this happens first
-  init(questions);
+function init(questions) {
+  inquirer
+    .prompt(
+      /* Pass your questions in here */
+      questions
+    )
+    .then(({ textColor, shape, text, shapeColor }) => {
+      console.log(textColor, shape, text, shapeColor);
+
+      switch (shape) {
+        case "Square":
+          shape = new Square();
+          console.log("Square");
+          break;
+
+        case "Triangle":
+          shape = new Triangle();
+          console.log("Triangle");
+          break;
+
+        default:
+          shape = new Circle();
+          console.log("Circle");
+          break;
+      }
+const svg = new Shape()
+svg.setColor(shapeColor)
+svg.setText(text)
+svg.setTextColor(color)
+      // Use user feedback for... whatever!!
+      writeToFile("examples/shape.svg", svg.render());
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+        console.log("Prompt couldn't be rendered in the current environment");
+      } else {
+        // Something else went wrong
+        console.log("Something else went wrong");
+      }
+    });
+}
+
+// Function call to initialize app-this happens first
+init(questions);

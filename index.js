8b094e9d5c2ad1b919e42c315e0b3c317ddb2402
DDesta25 <145198978+DDesta25@users.noreply.github.com>
2024-01-23 09:50:45 -0500

    got the switches working

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-const{Shape, Square,Triangle,Circle} = require("./lib/shapes");
+// const{Shape, Square,Triangle,Circle} = require("./lib/shapes");
 
 
 const questions = [
-  // users github user name
+ 
   {
     type: "input",
-    name: "text-color",
+    name: "textColor",
     message: "what color should it be?",
   },
-  // users email
+
   {
     type: "list",
     name: "shape",
-    message: "what color is the text?",
+    message: "which of the fallowing shapes would you like?",
     choices: ['Square','Triangle','Circle']
   },
-  // title of the project
+  
   {
     type: "input",
     name: "text",
@@ -26,7 +26,55 @@ const questions = [
   },
   {
     type: "input",
-    name: "shape-color",
+    name: "shapeColor",
     message: "what color should the shape-color be?",
   }]
   
+  function writeToFile(fileName, data) {
+  
+    
+    return fs.writeFileSync(fileName, data);
+  }
+// text-color,shape,text,shape-color
+  function init(questions) {
+    inquirer
+      .prompt(
+        /* Pass your questions in here */
+        questions
+      )
+      .then(({textColor,shape,text,shapeColor}) => {
+        console.log(textColor,shape,text,shapeColor);
+       
+        switch (shape) {
+            case "Square":
+            //   shape = new Case1();
+            console.log('Square');
+              break;
+  
+            case "Triangle":
+            //   shape = new Case2();
+            console.log('Triangle');
+              break;
+  
+            default:
+            //   shape = new Case3();
+            console.log('Circle');
+              break;
+          }
+          const svg = new SVG()
+        // Use user feedback for... whatever!!
+        writeToFile("shape.svg", svg.render({...answers}));
+      })
+      .catch((error) => {
+        if (error.isTtyError) {
+          // Prompt couldn't be rendered in the current environment
+          console.log("Prompt couldn't be rendered in the current environment");
+        } else {
+          // Something else went wrong
+          console.log("Something else went wrong");
+        }
+      });
+  }
+  
+  // Function call to initialize app-this happens first
+  init(questions);
